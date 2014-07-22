4fdfd03f92288de174f5b7bfa9ba42a957b0dba3
Sebastian KÃgler <sebas@kde.org>
2014-07-23 00:54:31 +0200

    Change license from GPL to LGPL
    This makes it LGPLv2+ with KDE e.V. exception, suitable for a
    framework.
    
    OK'ed by Marco Martin
    
    CCMAIL:mart@kde.org

--- src/qmlcontrols/kcoreaddons/kuserproxy.h
+++ src/qmlcontrols/kcoreaddons/kuserproxy.h
@@ -1,21 +1,23 @@
-/***************************************************************************
- *   Copyright 2014 Sebastian Kügler <sebas@kde.org>                       *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
- ***************************************************************************/
+/*
+Copyright 2013 Marco Martin <mart@kde.org>
+Copyright 2014 Sebastian Kügler <sebas@kde.org>
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) version 3, or any
+later version accepted by the membership of KDE e.V. (or its
+successor approved by the membership of KDE e.V.), which shall
+act as a proxy defined in Section 6 of version 3 of the license.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef KUSERPROXY_H
 #define KUSERPROXY_H
@@ -25,6 +27,38 @@
 #include <KDirWatch>
 #include <KUser>
 
+/**
+ * KUserProxy (exposed as KUser to the QML runtime) is an object allowing
+ * read-only access to the user's name, os and version and the configured
+ * user image. This object can be used to personalize user interfaces.
+ *
+ * Example usage:
+ * @code
+    import org.kde.kcoreaddons 1.0 as KCoreAddons
+    [...]
+
+    Item {
+        [...]
+        KCoreAddons.KUser {
+            id: kuser
+        }
+
+        Image {
+            id: faceIcon
+            source: kuser.faceIconPath
+            [...]
+        }
+
+        Text {
+            text: kuser.fullName
+            [...]
+        }
+    }
+    @endcode
+
+ * @short KUser provides read-only access to the user's personal information
+ * @see KUser
+ */
 class KUserProxy : public QObject
 {
     Q_OBJECT
@@ -39,14 +73,49 @@ public:
     KUserProxy(QObject *parent = 0);
     ~KUserProxy();
 
+
+    /**
+     * @return the full name of the user
+     * @see nameChanged
+     */
     QString fullName() const;
+
+    /**
+     * @return the user's login name
+     * @see nameChanged
+     *
+     */
     QString loginName() const;
+
+    /**
+     * @return the url of the user's configured image (including file:/)
+     * @see nameChanged
+     */
     QString faceIconPath() const;
+
+    /**
+     * @return pretty name indicating operating system and version
+     * @see nameChanged
+     */
     QString os();
+
+    /**
+     * @return the system's hostname
+     * @see nameChanged
+     */
     QString host() const;
 
 Q_SIGNALS:
+    /**
+     * signal that the user's name or login name changed
+     * @see fullName
+     * @see loginName
+     */
     void nameChanged();
+    /**
+     * signal that the user image changed
+     * @see faceIconPath
+     */
     void faceIconPathChanged();
 
 private:
