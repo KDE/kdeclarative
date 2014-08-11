f05b6c1577ee63c84a298b4b64c8e8d7285ab9a0
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2014-08-11 22:55:29 +0200

    Remove unused #include.

--- src/qmlcontrols/kcoreaddons/kuserproxy.cpp
+++ src/qmlcontrols/kcoreaddons/kuserproxy.cpp
@@ -19,8 +19,6 @@ You should have received a copy of the GNU Lesser General Public
 License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <unistd.h>
-
 #include "kuserproxy.h"
 #include <QFile>
 #include <QHostInfo>
