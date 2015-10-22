KDE/kdeclarative

commit 3792923639b1c480fd622f7d4d31f6f888c925b9

make pre-specified view size precede initial object size in QuickViewSharedEngine
REVIEW: 125719

src/quickaddons/quickviewsharedengine.cpp
@@ -65,6 +65,7 @@ public:
     KDeclarative::QmlObjectSharedEngine *qmlObject;
     QuickViewSharedEngine::ResizeMode resizeMode;
     QSize initialSize;
+
 };
 
 void QuickViewSharedEnginePrivate::executionFinished()
@@ -79,11 +80,13 @@ void QuickViewSharedEnginePrivate::executionFinished()
         return;
     }
 
-    item->setParentItem(q->contentItem());
-    initialSize = QSize(item->width(), item ->height());
-    q->resize(initialSize);
-    q->contentItem()->setWidth(item->width());
-    q->contentItem()->setHeight(item->height());
+    if(q->size().isEmpty()) {
+        item->setParentItem(q->contentItem());
+        initialSize = QSize(item->width(), item ->height());
+        q->resize(initialSize);
+        q->contentItem()->setWidth(item->width());
+        q->contentItem()->setHeight(item->height());
+    }
 
     syncResizeMode();
 }

commit 1c3f42afe17cc090372eaf45647d71495de12c19

fix ciritical regression for QuickViewSharedEngine by commit 3792923639b1c480fd622f7d4d31f6f888c925b9
view's item is not set if item's geometry is set by manual.

src/quickaddons/quickviewsharedengine.cpp
@@ -80,8 +80,9 @@ void QuickViewSharedEnginePrivate::executionFinished()
         return;
     }
 
+    item->setParentItem(q->contentItem());
+
     if(q->size().isEmpty()) {
-        item->setParentItem(q->contentItem());
         initialSize = QSize(item->width(), item ->height());
         q->resize(initialSize);
         q->contentItem()->setWidth(item->width());