0376d410cd16e31f009d805bfa19e93287b2dd65
Friedrich W. H. Kossebau <kossebau@kde.org>
2017-07-03 15:50:13 +0200

    API dox: fix @param to use name of parameter, not type

--- src/qmlcontrols/draganddrop/DeclarativeDropArea.h
+++ src/qmlcontrols/draganddrop/DeclarativeDropArea.h
@@ -56,28 +56,28 @@ public:
 Q_SIGNALS:
     /**
      * Emitted when the mouse cursor dragging something enters in the drag area
-     * @param DeclarativeDragDropEvent description of the dragged content
+     * @param event description of the dragged content
      * @see DeclarativeDragDropEvent
      */
     void dragEnter(DeclarativeDragDropEvent* event);
 
     /**
      * Emitted when the mouse cursor dragging something leaves the drag area
-     * @param DeclarativeDragDropEvent description of the dragged content
+     * @param event description of the dragged content
      * @see DeclarativeDragDropEvent
      */
     void dragLeave(DeclarativeDragDropEvent* event);
 
     /**
      * Emitted when the mouse cursor dragging something moves over the drag area
-     * @param DeclarativeDragDropEvent description of the dragged content
+     * @param event description of the dragged content
      * @see DeclarativeDragDropEvent
      */
     void dragMove(DeclarativeDragDropEvent *event);
 
     /**
      * Emitted when the user drops something in the area
-     * @param DeclarativeDragDropEvent description of the dragged content
+     * @param event description of the dragged content
      * @see DeclarativeDragDropEvent
      */
     void drop(DeclarativeDragDropEvent* event);
