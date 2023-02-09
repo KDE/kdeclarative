1dd932d89e734b9ec88b5234a12235c6fd7664d1
Volker Krause <vkrause@kde.org>
2023-02-09 18:14:53 +0100

    Remove Qt 5 leftovers

--- src/qmlcontrols/kquickcontrolsaddons/qpixmapitem.h
+++ src/qmlcontrols/kquickcontrolsaddons/qpixmapitem.h
@@ -68,11 +68,7 @@ Q_SIGNALS:
     void paintedHeightChanged();
 
 protected:
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    void geometryChanged(const QRectF &newGeometry, const QRectF &oldGeometry) override;
-#else
     void geometryChange(const QRectF &newGeometry, const QRectF &oldGeometry) override;
-#endif
 
 private:
     QPixmap m_pixmap;
