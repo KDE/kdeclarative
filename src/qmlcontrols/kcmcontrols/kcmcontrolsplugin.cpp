8eb9c7cbd0ad048b09dfad65fb864f8a59773194
Alexander Lohnau <alexander.lohnau@gmx.de>
2023-03-12 08:42:41 +0100

    Remove ConfigModule/ManagedConfigModule classes
    Those were imported and refactored in KCMUtils, see https://invent.kde.org/frameworks/kcmutils/-/merge_requests/121
    
    Task: https://phabricator.kde.org/T12127

--- src/qmlcontrols/kcmcontrols/kcmcontrolsplugin.cpp
+++ src/qmlcontrols/kcmcontrols/kcmcontrolsplugin.cpp
@@ -8,13 +8,11 @@
 
 #include "settinghighlighterprivate.h"
 #include "settingstateproxy.h"
-#include <KQuickAddons/ConfigModule>
 
 void KCMControlsPlugin::registerTypes(const char *uri)
 {
     Q_ASSERT(uri == QByteArray("org.kde.kcm"));
 
-    qmlRegisterUncreatableType<KQuickAddons::ConfigModule>(uri, 1, 0, "ConfigModule", QLatin1String("Do not create objects of type ConfigModule"));
     qmlRegisterType<SettingStateProxy>(uri, 1, 3, "SettingStateProxy");
     qmlRegisterType<SettingHighlighterPrivate>("org.kde.kcm.private", 1, 3, "SettingHighlighterPrivate");
 }
