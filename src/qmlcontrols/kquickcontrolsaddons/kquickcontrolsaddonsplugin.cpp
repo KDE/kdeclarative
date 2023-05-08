44b69f3ede30a18b049130dc3b24a1a3ad5eb3fe
Alexander Lohnau <alexander.lohnau@gmx.de>
2023-05-08 18:01:18 +0200

    Remove KCMShell class from kquickaddons
    In https://invent.kde.org/frameworks/kcmutils/-/merge_requests/146, this
    got imported and refactored to KCMUtils.
    This is also the more natural place for it.

--- src/qmlcontrols/kquickcontrolsaddons/kquickcontrolsaddonsplugin.cpp
+++ src/qmlcontrols/kquickcontrolsaddons/kquickcontrolsaddonsplugin.cpp
@@ -12,19 +12,10 @@
 #include "columnproxymodel.h"
 #include "eventgenerator.h"
 #include "fallbacktaphandler.h"
-#include "kcmshell.h"
 #include "mimedatabase.h"
 #include "mouseeventlistener.h"
 #include "qimageitem.h"
 #include "qpixmapitem.h"
-
-static QObject *kcmshell_singleton_provider(QQmlEngine *engine, QJSEngine *scriptEngine)
-{
-    Q_UNUSED(engine);
-    Q_UNUSED(scriptEngine);
-
-    return new KCMShell();
-}
 
 void KQuickControlsAddonsPlugin::registerTypes(const char *uri)
 {
@@ -38,7 +29,6 @@ void KQuickControlsAddonsPlugin::registerTypes(const char *uri)
     qmlRegisterType<ColumnProxyModel>(uri, 2, 0, "ColumnProxyModel");
     qmlRegisterType<Clipboard>(uri, 2, 0, "Clipboard");
     qmlRegisterType<MimeDatabase>(uri, 2, 0, "MimeDatabase");
-    qmlRegisterSingletonType<KCMShell>(uri, 2, 0, "KCMShell", kcmshell_singleton_provider);
     qmlRegisterType<EventGenerator>(uri, 2, 0, "EventGenerator");
     qmlRegisterUncreatableType<FallbackTapHandlerMouseEvent>(uri,
                                                              2,
